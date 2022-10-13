315ee6cc92bf137e11322f51ff1c9a487b968676
mhiztamem <eliaguochu@gmail.com>
2022-10-13 07:34:12 -0700

    making the steady progress

---
+++ 0x00-hello_world/4-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * trying to use puts out here
+ */
+int main()
+{
+	/*inputing the output*/
+	char myVar[] = "Programming is like building a multilingual puzzle";
+	/*using puts to output myVar*/
+	puts(myVar);
+
+	return (0);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc 4-main.c -o my_prog

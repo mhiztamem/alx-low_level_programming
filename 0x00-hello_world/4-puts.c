f5e71479d93d4d7da51c69f9fa77af0b1d77235c
mhiztamem <eliaguochu@gmail.com>
2022-10-13 07:50:53 -0700

    working hard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-gcc 4-main.c -o my_prog
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*inputing the output*/
+	char myVar[] = "Programming is like building a multilingual puzzle";
+	/*using puts to output myVar*/
+	puts(myVar);
+
+	return (0);
+}

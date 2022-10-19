6f0035dac98fccadac0a6ea6083c4c7fcd6863ad
mhiztamem <eliaguochu@gmail.com>
2022-10-18 18:44:03 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
  * print_times_table - prints the times table up to the integer input
  * @n: integer input
  *
  * Return: void
  */
-
 void print_times_table(int n)
 {
 	int a, b, c, d, e, f;
 
 	if (n >= 0 && n < 15)
-	{
-		for (a = 0; a <=  n; a++)
-		{
-			for (b = 0; b <= n; b++)
+	{	for (a = 0; a <=  n; a++)
+		{		for (b = 0; b <= n; b++)
 			{
 				c = a * b;
 				d = c / 100;
@@ -50,7 +46,4 @@ void print_times_table(int n)
 				}
 				else
 					_putchar('\n');
-			}
-		}
-	}
-}
+				}	}}}

bd74337c2321feb0008cd741efb4111d3bd94a76
mhiztamem <eliaguochu@gmail.com>
2022-10-31 06:32:57 -0700

    my commits

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include "hmain.h"
+#include "main.h"
 /**
  * _memset - fills memory with a constant byte
  *
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,18 +10,31 @@
  * @accept, or NULL if no such byte is found.
  */
 
-char *_strpbrk(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-	int i;
-
-	while (*s)
+	unsigned int i, z, count, checker;
+	i = 0;
+	z = 0;
+	checker = 0;
+	count = 0;
+	while (s[i] != '\0')
 	{
-		for (i = 0; accept[i]; i++)
+		z = 0;
+		checker = 0;
+		while (accept[z] != '\0')
 		{
-			if (*s == accept[i])
-				return (s);
+			if (accept[z] == s[i])
+			{
+				count++;
+				checker = 1;
+			/* Break to early increase efficiency */
+				break;
+			}
+			z++;
 		}
-		s++;
+		if (checker == 0)
+			return (count);
+		i++;
 	}
-	return (NULL);
+	return (count);
 }
